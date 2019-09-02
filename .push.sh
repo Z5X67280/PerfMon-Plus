2d168239de9277d0bce5f2f600c3cf2a51e44f90
Keternal <Z5X67280@163.com>
2019-09-02 13:21:09 +0800

    perfmon: Hmmmmmmmz
    perfmon: A bit opt
    
    Signed-off-by: Keternal <Z5X67280@163.com>
    
    travis: Better file name
    
    Signed-off-by: Keternal <Z5X67280@163.com>
    
    perfmon: drop zipalign
    
    Signed-off-by: Keternal <Z5X67280@163.com>
    
    perfmon: Hmm
    
    Signed-off-by: Keternal <Z5X67280@163.com>

--- .push.sh
+++ .push.sh
@@ -1,8 +1,10 @@
 export BOT_API_KEY="963254339:AAGF81vp_kdZ-eXEV_xoOuJDgytQpont9y4"
 export TELEGRAM_ID="-1001268516549"
 
+mv app/build/outputs/apk/release/app-release-unsigned.apk app/build/outputs/apk/release/PerfMon-Plus-Translated-UNSIGNED.apk
+
 function push_package() {
-	JIP="app/build/outputs/apk/release/app-release-unsigned.apk"
+	JIP="app/build/outputs/apk/release/PerfMon-Plus-Translated-UNSIGNED.apk"
 	curl -F document=@$JIP  "https://api.telegram.org/bot$BOT_API_KEY/sendDocument" \
 	     -F chat_id="$TELEGRAM_ID"
 }
